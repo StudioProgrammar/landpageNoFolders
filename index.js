808f1fe0841208aefbd44e7494e6009584160723
StudioProgrammer <109485616+StudioProgrammar@users.noreply.github.com>
2023-08-12 08:13:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var box = document.getElementById('check')
+let checker = false;
 
 
 //works to prevent any background scrolling for mobile
 //even works when the menu isn't in a fixed position
 
 box.addEventListener('click' , () => {
-    document.body.style.position = 'fixed'
+
+    if(checker == false) {
+          document.body.style.position = 'fixed'  
+    } else if (checker == true) {
+        document.body.style.position = 'unset'
+    }
 })
